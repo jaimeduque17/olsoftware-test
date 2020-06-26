972c60d87664bd69b7befb67c8141b816308ec38
Jaime Duque <jaime.duque@estratek.com.co>
2020-06-26 01:05:04 -0500

    header component is created

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useUser } from 'reactfire';
 import Auth from './components/Auth';
+import Header from './components/Header';
 import Users from './pages/Users';
 
 function App() {
@@ -8,13 +9,10 @@ function App() {
   const user = useUser();
 
   return (
-    <div className="row">
-      <div className="col-md-8 offset-md-2">
-        <h1>OL Software</h1>
-        {user && <p>{user.email}</p>}
-        <Auth />
-        <Users />
-      </div>
+    <div>
+      {user ? <Header name={user.email} /> : null}
+      {!user && <Auth />}
+      {user ? <Users /> : null}
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import 'firebase/auth';
+import { useFirebaseApp, useUser } from 'reactfire';
+
+const Header = ({ name }) => {
+
+    const firebase = useFirebaseApp();
+
+    const logout = async () => {
+        await firebase.auth().signOut();
+    }
+
+    return (
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <i className="fas fa-bars text-primary mr-4" style={{ fontSize: '24px' }}></i>
+            <strong className="text-primary">Prueba Front-end</strong>
+            <div className="navbar-nav mr-auto" />
+            <i className="fas fa-user-circle text-secondary mr-4" style={{ fontSize: '36px' }}></i>
+            <span>{name}</span>
+            <button className="btn text-primary ml-4" type="submit" onClick={logout}>
+                <i className="fas fa-sign-out-alt" style={{ fontSize: '24px' }}></i>
+            </button>
+        </nav>
+    );
+}
+
+export default Header;
